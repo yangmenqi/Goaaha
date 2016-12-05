ee057abf8edc5d03dc174986941664300843095d
小妖精停一下 <775323759@qq.com>
2016-12-05 16:24:38 +0800

    聊天页面中，fragment嵌套fragment——孔还庆

--- Goaaha/app/src/main/java/com/example/lenovo/goaaha/ChatFragment.java
+++ Goaaha/app/src/main/java/com/example/lenovo/goaaha/ChatFragment.java
@@ -14,12 +14,12 @@ import android.widget.LinearLayout;
 /**
  * Created by Lenovo on 2016/11/7.
  */
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements View.OnClickListener{
 
 
     private Chat chat_fragment;
     private Contacts contacts_fragment;
-
+    private View rootView;
     private Fragment fragments[];
     private Button[] mTabs;
     private int index=0;
@@ -27,29 +27,30 @@ public class ChatFragment extends Fragment {
     private int currentTabIndex=0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.layout_chatfragment, container, false);
         chat_fragment = new Chat();
         contacts_fragment = new Contacts();
         fragments = new Fragment[]{chat_fragment, contacts_fragment};
         getChildFragmentManager().beginTransaction().add(R.id.id_in_contaner, chat_fragment).add(R.id.id_in_contaner, contacts_fragment).hide(contacts_fragment).show(chat_fragment).commit();
-        mTabs = new Button[2];
-        mTabs[0] = (Button) container.findViewById(R.id.id_btn_chat);
-        mTabs[1] = (Button) container.findViewById(R.id.id_btn_contacts);
-        mTabs[0].setSelected(true);
-
-        return inflater.inflate(R.layout.layout_chatfragment, container, false);
+        getViews();
+        setListener();
+        return rootView;
     }
 
-    /*private void getViews() {
+    private void getViews() {
 
         mTabs = new Button[2];
-        mTabs[0] = (Button) findViewById(R.id.id_btn_chat);
-        mTabs[1] = (Button) getActivity().findViewById(R.id.id_btn_contacts);
+        mTabs[0] = (Button) rootView.findViewById(R.id.id_btn_chat);
+        mTabs[1] = (Button) rootView.findViewById(R.id.id_btn_contacts);
         mTabs[0].setSelected(true);
-        registerForContextMenu(mTabs[1]);
-    }*/
 
-    public void onTabClicked(View view) {
-        switch (view.getId()) {
+    }
+    private void setListener() {
+        mTabs[0].setOnClickListener(this);
+        mTabs[1].setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        switch (v.getId()){
             case R.id.id_btn_chat:
                 index = 0;
                 break;
@@ -70,4 +71,27 @@ public class ChatFragment extends Fragment {
         mTabs[index].setSelected(true);
         currentTabIndex = index;
     }
+
+    /*public void onTabClicked(View view) {
+        switch (view.getId()) {
+            case R.id.id_btn_chat:
+                index = 0;
+                break;
+            case R.id.id_btn_contacts:
+                index = 1;
+                break;
+        }
+        if (currentTabIndex != index) {
+            FragmentTransaction trx =  getChildFragmentManager().beginTransaction();
+            trx.hide(fragments[currentTabIndex]);
+            if (!fragments[index].isAdded()) {
+                trx.add(R.id.id_in_contaner, fragments[index]);
+            }
+            trx.show(fragments[index]).commit();
+        }
+        mTabs[currentTabIndex].setSelected(false);
+        // 把当前tab设为选中状态
+        mTabs[index].setSelected(true);
+        currentTabIndex = index;
+    }*/
 }

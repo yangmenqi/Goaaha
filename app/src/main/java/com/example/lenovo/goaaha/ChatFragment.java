026ede9808f8e57bb21373814d9dc2b875db882c
yangmengqi <719944159@qq.com>
2016-12-05 18:44:22 +0800

    货币类型的选择

---
+++ app/src/main/java/com/example/lenovo/goaaha/ChatFragment.java
@@ -0,0 +1,27 @@
+package com.example.lenovo.goaaha;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lenovo on 2016/11/7.
+ */
+public class ChatFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_chat,container,false);
+    }
+}
